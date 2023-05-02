362269340789d140af7e7a481dea48f2dccbd7e5
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-05-02 02:38:34 +0000

    did extra (main)

---
+++ go_app/main.go
@@ -0,0 +1,31 @@
+// Created By: Lamees Hemdan
+// Created On: May 2023
+//
+// This file contains the main function for the go_app application.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// This program gets the age and let the person know which movies they can watch.
+	var age string
+
+	// input
+	fmt.Println("Enter your age: ")
+	fmt.Scanln(&age)
+
+	if age >= "17" {
+		fmt.Println("You can watch R rated movies!")
+	} else if age >= "13" {
+		fmt.Println("You can watch PG-13 movies!")
+	} else if age >= "5" {
+		fmt.Println("Go!")
+	} else {
+		fmt.Println("Sorry, you are too young for most movies.")
+	}
+
+	fmt.Println("\nDone.")
+}
