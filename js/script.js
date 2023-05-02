b8a02965fa62fec8787433f052dd7f3a775a6f4c
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-05-02 02:07:02 +0000

    added favicon

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Lamees Hemdan All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lamees Hemdan
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked () {
+  // This program gets the age and let the person know which movies they can watch.
+
+  // input
+  const age = parseInt(document.getElementById("age").value)
+
+  // process
+  if (age >= 17) {
+    // output
+    document.getElementById('age-entered').innerHTML = "You can watch R movies alone."
+  }
+  else if (age >= 13) {
+    // output
+    document.getElementById('age-entered').innerHTML = "You can watch PG-13 movies alone."
+  }
+  else if (age >= 5) {
+    // output
+    document.getElementById('age-entered').innerHTML = "You can watch G or PG movies alone."
+  }
+  else {
+    // output
+    document.getElementById('age-entered').innerHTML = "Sorry, you are too young for most things."
+  }
+}
